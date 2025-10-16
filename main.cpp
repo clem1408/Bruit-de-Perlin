b6dd2ed9822b16dec82ac4ece4f784849ad8890c
ElCald <eliot.cal@gmail.com>
2025-10-16 10:30:24 +0200

    grid generation

--- main.cpp
+++ main.cpp
@@ -15,14 +15,46 @@ int main(int argc, char* argv[]) {
     }
 
     int img_width=1, img_height=1;
+    int grid_width=2, grid_height=2;
 
     img_width = atoi(argv[1]);
     img_height = atoi(argv[2]);
 
-    // Lire l'image
-    // cv::Mat image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+    grid_width = img_width +1;
+    grid_height = img_height +1;
 
-    cv::Mat img(img_width, img_height, CV_8UC1, cv::Scalar(70));
+    // Création image
+    cv::Mat img(img_width, img_height, CV_8UC1, cv::Scalar(0));
+
+
+    // Création de la grille de vecteur
+    double*** grid = (double***)malloc(sizeof(double**)*grid_width);
+    for(int i=0; i<grid_width; i++){
+        grid[i] = (double**)malloc(sizeof(double*)*grid_height);
+    }
+
+    for(int i=0; i<grid_width; i++){
+        for(int j=0; j<grid_height; j++){
+            grid[i][j] = (double*)malloc(sizeof(double)*2);
+        }
+    }
+
+
+    // Générateur de nombres aléatoires
+    cv::RNG rng(cv::getTickCount());
+
+
+    // Remplir la grille de vecteur
+    for (int y = 0; y < grid_height; y++) {
+        for (int x = 0; x < grid_width; x++) {
+            double theta = rng.uniform(0.0, 2 * CV_PI); // Génération d'un angle aléatoire
+
+            grid[y][x][0] = std::cos(theta); // x
+            grid[y][x][1] = std::sin(theta); // y
+        }
+    }
+
+
 
     if (img.empty()) { // Vérification de si l'image existe
         cerr << "Erreur de lecture de l'image!" << endl;
